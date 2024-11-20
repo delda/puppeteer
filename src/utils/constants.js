50a5407185ce8f2b57779e26634ef5f425331303
Davide Dell'Erba <delda@virgilio.it>
2024-11-20 16:23:09 +0100

    Forgotten file

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const gentlenessTable = ['attaccabrighe', 'scontrosa', 'gradevole', 'simpatica', 'popolare'];
+export const aggressivenessTable = ['irascibile', 'impulsiva', 'equilibrata', 'calma', 'tranquilla'];
+export const honestyTable = ['infame', 'disonesta', 'onesta', 'retta', 'virtuosa'];
+export const formTable = ['inesistente', 'disastroso', 'tremendo', 'scarso', 'debole', 'insufficiente', 'accettabile', 'buono', 'eccellente'];
+export const staminaTable = ['inesistente', 'disastroso', 'tremendo', 'scarso', 'debole', 'insufficiente', 'accettabile', 'buono', 'eccellente', 'formidabile'];
+export const skills = ['inesistente', 'disastroso', 'tremendo', 'scarso', 'debole', 'insufficiente', 'accettabile', 'buono', 'eccellente', 'formidabile', 'straordinario', 'splendido', 'magnifico', 'fuoriclasse', 'sovrannaturale', 'titanico', 'extraterrestre', 'mitico', 'magico', 'utopico', 'divino'];
+
+Object.freeze({gentlenessTable, aggressivenessTable, honestyTable, formTable, staminaTable, skills});
+
